a348d11ca56dac093f9d8519c523eefe733faa1b
Renato Rodrigues <renato@renatorodrigues.com>
2020-02-08 20:50:12 -0300

    Add eslint + eslint-typescript

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+  },
+};
