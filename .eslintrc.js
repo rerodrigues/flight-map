94897a165e92cccfd5ff4a91fa7087e591d9d031
Renato Rodrigues <renato@renatorodrigues.com>
2020-02-08 21:07:01 -0300

    Add eslint-config-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
+    'airbnb',
+    'airbnb/hooks',
     'plugin:json/recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ['json'],
+  plugins: ['@typescript-eslint', 'json'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -13,13 +14,28 @@ module.exports = {
       jsx: true,
     },
   },
+  env: {
+    es2020: true,
+    browser: true,
+    jest: true,
+  },
   settings: {
     react: {
       version: 'detect',
     },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
   },
   rules: {
     'json/*': 'error',
+    'react/jsx-filename-extension': ["error", { extensions: ['.ts', '.tsx'] }],
+    'import/extensions': ['error', 'never', { 'svg': 'always' }],
+    'import/no-extraneous-dependencies': ["error", { devDependencies: ['**/{test,spec}.tsx?'] }],
+    'react/jsx-one-expression-per-line': 'off',
     '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
   },
 };
