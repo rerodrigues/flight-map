3f21c2a40a0f8903d892cb7792075240eae3f080
Renato Rodrigues <renato@renatorodrigues.com>
2020-02-08 22:50:01 -0300

    Add router + connected router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
-import store from './store';
+import store, { history } from './store';
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,17 +11,27 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            )}
+          />
+        </Switch>
+      </ConnectedRouter>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,23 @@
 import createSagaMiddleware from 'redux-saga';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 
 import reducers from './reducers';
 // import sagas from './sagas';
 
 const composeEnhancers = composeWithDevTools({});
 
+export const history = createBrowserHistory();
+
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = applyMiddleware(sagaMiddleware);
+const connectedRouterMiddleware = routerMiddleware(history);
+const middlewares = applyMiddleware(sagaMiddleware, connectedRouterMiddleware);
 
 const rootReducers = combineReducers({
   ...reducers,
+  router: connectRouter(history),
 });
 
 // sagas.forEach(sagaMiddleware.run);
