84875fac0e4dd32958c657daf59d8689265fc7ef
Renato Rodrigues <renato.sp@gmail.com>
2020-03-16 03:37:09 -0300

    Airport Popup WIP

--- src/components/Airports/components/AirportPopup/index.tsx
+++ src/components/Airports/components/AirportPopup/index.tsx
@@ -1,23 +1,46 @@
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import React from 'react';
-import { Popup } from 'react-leaflet';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import { Popup } from 'react-leaflet';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { Airport } from '../../../../services/airports/types';
 
 interface AirportPopupProps {
   airport: Airport;
 }
 
-const AirportPopup: React.FC<AirportPopupProps> = ({ airport }: AirportPopupProps) => (
-  <Popup>
-    <h3>Airport: {airport.name}</h3>
-    <h4>
-      Country:
-      <Link to={`/airports/country/${airport.country}`}>{airport.country}</Link>
-    </h4>
-    <h4>
-      <Link to={`/flights/airport/${airport.icao}`}>All Flights from this airport</Link>
-    </h4>
-  </Popup>
-);
+const useStyles = makeStyles({
+  root: {},
+  subtitle: {
+    marginBottom: 12,
+  },
+});
+
+const AirportPopup: React.FC<AirportPopupProps> = ({ airport }: AirportPopupProps) => {
+  const classes = useStyles();
+
+  return (
+    <Popup closeButton={false}>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {airport.name}
+          </Typography>
+          <Typography className={classes.subtitle} color="textSecondary">
+            Country: <Link to={`/airports/country/${airport.country}`}>{airport.country}</Link>
+          </Typography>
+          <Typography variant="body2" component="p">
+            <Link to={`/flights/airport/${airport.icao}`}>All Flights from this airport</Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Popup>
+  );
+};
 
 export default AirportPopup;
