24c93ebac34c581ae25d32cac3b65e7b6c1e788a
Renato Rodrigues <renato.sp@gmail.com>
2020-06-10 15:41:24 -0300

    style fixes

--- src/components/Airport/components/DetailsCard/styles.ts
+++ src/components/Airport/components/DetailsCard/styles.ts
@@ -4,10 +4,5 @@ export const useStyles = makeStyles(() => ({
   root: {
     height: '100vh',
     overflow: 'auto',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    zIndex: 1000,
-    width: 400,
   },
 }));
