16d3805827d8a4cfa78d5af59ed88e0fa64fda09
root <root@Tardis.localdomain>
2020-09-05 01:05:36 -0300

    Component Header created

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Flights, Airports, Menu } from '..';
-import { Airport } from '../../containers';
+import { Airport, Header } from '../../containers';
 
 export const Routes: React.FC = () => (
-  <Switch>
-    <Route exact path="/airports" component={Airports} />
-    <Route exact path="/airports/country/:countryId" component={Airports} />
-    <Route exact path="/flights" component={Flights} />
-    <Route exact path="/flights/company/:companyCode" component={Flights} />
-    <Route exact path="/flights/airport/:icaoCode" component={Flights} />
-    <Route exact path="/airport/:icao" component={Airport} />
-    <Route path="/menu" component={Menu} />
-    <Route path="/" component={Airports} />
-  </Switch>
+  <>
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/airports" component={Airports} />
+          <Route exact path="/airports/country/:countryId" component={Airports} />
+          <Route exact path="/flights" component={Flights} />
+          <Route exact path="/flights/company/:companyCode" component={Flights} />
+          <Route exact path="/flights/airport/:icaoCode" component={Flights} />
+          <Route exact path="/airport/:icao" component={Airport} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/" component={Airports} />
+        </Switch>
+      </main>
+    </div>
+  </>
 );
 
 export default Routes;
